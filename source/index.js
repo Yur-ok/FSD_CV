2b6c0cdc58939ac4b88a9c06646f6f6b90af6765
Yur-ok <Materik-r@yandex.ru>
2018-10-23 13:05:41 +0700

    refactor full project.
    added variables.styl & mixins folder for pug mixin

--- source/index.js
+++ source/index.js
@@ -1,8 +1,1 @@
-// require('./style.styl');
-// require('1_bio/bio_style.styl');
-
 import './style.styl';
-
-// import '2_profile/PROFILE.png';
-// import '3_contact/Phone.png';
-// import '4_skills/SKILLS.png';
